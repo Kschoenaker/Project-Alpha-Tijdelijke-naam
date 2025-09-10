377dddfaef50ff951dbb424f77ad721e3564aaa3
Kschoenaker <0930147@hr.nl>
2025-09-10 13:48:37 +0100

    class Quest aangemaakt

---
+++ Quests.cs
@@ -0,0 +1,50 @@
+public class Quest
+{
+    public int QuestId;
+    public string Name;
+    public string Descriptions;
+    public bool QuestStarted;
+
+
+
+    public Quest(string name, int questId, string Descriptions)
+    {
+        QuestId = questId;
+        Name = name;
+        QuestMassage = questMassage;
+        QuestStarted = false;
+
+    }
+
+
+
+    // talk to npc
+
+    // receive quest 
+
+    // accept or dineid
+
+    // if accept
+    // start quest massage
+
+    // if denied 
+    // maybe later massage
+
+    // if quest completed
+    // reveive reward 
+
+
+
+}
+
+    // public List<> massages;
+    // // quest start massage
+
+    // // quest eccept massage
+
+    // // quest denied massage
+
+    // // quest completed massage
+    // public bool questCompleted;
+    // public string reward;
+
